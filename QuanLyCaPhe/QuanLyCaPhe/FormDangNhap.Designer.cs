debbc39e24b2dc6dd5e109f4bb2982d0881b28b5
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-05-04 09:22:30 +0700

    Vừa mới sửa câu truy vấn SQL
    - Không tạo lớp con của nhân viên, thêm bảng nhân viên.
    - Đang tiếp tục Cập Nhật Form Đăng Nhập và Form giao diện chính.

---
+++ QuanLyCaPhe/QuanLyCaPhe/FormDangNhap.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace QuanLyCaPhe
+{
+    partial class FormDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMatKhau);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTen);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(25, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(624, 214);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin đăng nhập";
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMatKhau.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtMatKhau.Location = new System.Drawing.Point(218, 117);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(388, 41);
+            this.txtMatKhau.TabIndex = 3;
+            this.txtMatKhau.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // txtTen
+            // 
+            this.txtTen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTen.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtTen.Location = new System.Drawing.Point(218, 49);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(388, 41);
+            this.txtTen.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên người dùng:";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(413, 245);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(212, 45);
+            this.btnThoat.TabIndex = 11;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(64, 245);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(212, 45);
+            this.btnDangNhap.TabIndex = 10;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // FormDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 315);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangNhap);
+            this.Name = "FormDangNhap";
+            this.Text = "Đăng Nhập";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnDangNhap;
+    }
+}
+
--- Query_Windows Prgamming.sql
+++ Query_Windows Prgamming.sql
@@ -8,16 +8,14 @@ use QuanLyCaPhe
 
 drop table if exists KhachHang
 drop table if exists NhanVien
-drop table if exists NhanVienPhucVu
-drop table if exists NhanvienThuNgan
-drop table if exists NhanVienQuanLy
 drop table if exists ThanhPho
+drop table if exists DangNhap
 
 create table KhachHang (
 	MaKH varchar(20) not null,
 	HoKH varchar(20),
 	TenKH varChar(20),
-	NgaySinh datetime,
+	NgaySinh date,
 	SDT varchar(20),
 	DiaChi varchar(30),
 	primary key(MaKH)
@@ -27,11 +25,17 @@ create table NhanVien(
 	MaNV varchar(20) not null,
 	HoNV varchar(20),
 	TenNV varchar(20),
-	NgaySinh datetime,
+	NgaySinh date,
 	SDT varchar(20),
 	DiaChi varchar(30),
 	NgayBD datetime,
 	primary key(MaNV)
+)
+
+create table DangNhap(
+	TaiKhoan varchar(20),
+	MatKhau varchar(20)
+	primary key(TaiKhoan)
 )
 
 create table ThanhPho (
@@ -42,7 +46,8 @@ create table ThanhPho (
 
 delete from KhachHang
 delete from  NhanVien
---delete from  NhanVienPhucVu
---delete from  NhanvienThuNgan
---delete from  NhanVienQuanLy
 delete from ThanhPho
+delete from DangNhap
+
+insert into KhachHang values('KH1', 'Nguyen Xuan', 'Huy', '1997-12-05', '0904567841', '92 Hai Ba Trung Ho Chi Minh')
+
