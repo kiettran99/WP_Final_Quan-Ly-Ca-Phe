a2a38e026be5df26c282bc08097cbb3e3f5757fa
nguyendat1234 <47876667+nguyendat1234@users.noreply.github.com>
2019-05-04 10:11:29 +0700

    giaodienkhachhang

---
+++ QuanLyCaPhe/QuanLyCaPhe/FormKhachHang.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace QuanLyCaPhe
+{
+    partial class FormKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtXoa = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbTen = new System.Windows.Forms.Label();
+            this.lbDiaChi = new System.Windows.Forms.Label();
+            this.lbSDT = new System.Windows.Forms.Label();
+            this.lbHo = new System.Windows.Forms.Label();
+            this.lbNS = new System.Windows.Forms.Label();
+            this.lbMaKH = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbKH = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btLuu = new System.Windows.Forms.Button();
+            this.btThem = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtXoa
+            // 
+            this.BtXoa.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtXoa.Image = global::QuanLyCaPhe.Properties.Resources._3Xoa;
+            this.BtXoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtXoa.Location = new System.Drawing.Point(579, 168);
+            this.BtXoa.Name = "BtXoa";
+            this.BtXoa.Size = new System.Drawing.Size(81, 43);
+            this.BtXoa.TabIndex = 0;
+            this.BtXoa.Text = "Xóa";
+            this.BtXoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtXoa.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(495, 143);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(495, 215);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(232, 22);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(204, 215);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(161, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(495, 182);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(205, 22);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 22);
+            this.textBox2.TabIndex = 15;
+            // 
+            // lbTen
+            // 
+            this.lbTen.AutoSize = true;
+            this.lbTen.Location = new System.Drawing.Point(79, 218);
+            this.lbTen.Name = "lbTen";
+            this.lbTen.Size = new System.Drawing.Size(119, 17);
+            this.lbTen.TabIndex = 6;
+            this.lbTen.Text = "Tên Khách Hàng:";
+            // 
+            // lbDiaChi
+            // 
+            this.lbDiaChi.AutoSize = true;
+            this.lbDiaChi.Location = new System.Drawing.Point(387, 218);
+            this.lbDiaChi.Name = "lbDiaChi";
+            this.lbDiaChi.Size = new System.Drawing.Size(53, 17);
+            this.lbDiaChi.TabIndex = 7;
+            this.lbDiaChi.Tag = "";
+            this.lbDiaChi.Text = "Địa Chỉ";
+            // 
+            // lbSDT
+            // 
+            this.lbSDT.AutoSize = true;
+            this.lbSDT.Location = new System.Drawing.Point(387, 182);
+            this.lbSDT.Name = "lbSDT";
+            this.lbSDT.Size = new System.Drawing.Size(102, 17);
+            this.lbSDT.TabIndex = 8;
+            this.lbSDT.Tag = "";
+            this.lbSDT.Text = "Số Điện Thoại:";
+            // 
+            // lbHo
+            // 
+            this.lbHo.AutoSize = true;
+            this.lbHo.Location = new System.Drawing.Point(79, 182);
+            this.lbHo.Name = "lbHo";
+            this.lbHo.Size = new System.Drawing.Size(112, 17);
+            this.lbHo.TabIndex = 9;
+            this.lbHo.Text = "Họ Khách Hàng:";
+            // 
+            // lbNS
+            // 
+            this.lbNS.AutoSize = true;
+            this.lbNS.Location = new System.Drawing.Point(387, 143);
+            this.lbNS.Name = "lbNS";
+            this.lbNS.Size = new System.Drawing.Size(77, 17);
+            this.lbNS.TabIndex = 10;
+            this.lbNS.Tag = "";
+            this.lbNS.Text = "Ngày Sinh:";
+            // 
+            // lbMaKH
+            // 
+            this.lbMaKH.AutoSize = true;
+            this.lbMaKH.Location = new System.Drawing.Point(79, 143);
+            this.lbMaKH.Name = "lbMaKH";
+            this.lbMaKH.Size = new System.Drawing.Size(117, 17);
+            this.lbMaKH.TabIndex = 11;
+            this.lbMaKH.Text = "Mã Khách Hàng: ";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.BtXoa);
+            this.panel2.Controls.Add(this.btSua);
+            this.panel2.Controls.Add(this.btLuu);
+            this.panel2.Controls.Add(this.btThem);
+            this.panel2.Location = new System.Drawing.Point(53, 131);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(827, 319);
+            this.panel2.TabIndex = 4;
+            // 
+            // lbKH
+            // 
+            this.lbKH.AutoSize = true;
+            this.lbKH.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbKH.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbKH.Location = new System.Drawing.Point(204, 6);
+            this.lbKH.Name = "lbKH";
+            this.lbKH.Size = new System.Drawing.Size(351, 33);
+            this.lbKH.TabIndex = 0;
+            this.lbKH.Text = "DANH MỤC KHÁCH HÀNG";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 16;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbKH);
+            this.panel1.Location = new System.Drawing.Point(53, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(827, 52);
+            this.panel1.TabIndex = 5;
+            // 
+            // btSua
+            // 
+            this.btSua.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSua.Image = global::QuanLyCaPhe.Properties.Resources._2Sua;
+            this.btSua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btSua.Location = new System.Drawing.Point(427, 168);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(74, 43);
+            this.btSua.TabIndex = 0;
+            this.btSua.Text = "Sửa";
+            this.btSua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btSua.UseVisualStyleBackColor = true;
+            // 
+            // btLuu
+            // 
+            this.btLuu.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLuu.Image = global::QuanLyCaPhe.Properties.Resources.Luu;
+            this.btLuu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btLuu.Location = new System.Drawing.Point(296, 168);
+            this.btLuu.Name = "btLuu";
+            this.btLuu.Size = new System.Drawing.Size(80, 43);
+            this.btLuu.TabIndex = 0;
+            this.btLuu.Text = "Lưu";
+            this.btLuu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btLuu.UseVisualStyleBackColor = true;
+            // 
+            // btThem
+            // 
+            this.btThem.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThem.Image = global::QuanLyCaPhe.Properties.Resources._1Them;
+            this.btThem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btThem.Location = new System.Drawing.Point(121, 168);
+            this.btThem.Name = "btThem";
+            this.btThem.Size = new System.Drawing.Size(99, 43);
+            this.btThem.TabIndex = 0;
+            this.btThem.Text = "Thêm";
+            this.btThem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btThem.UseVisualStyleBackColor = true;
+            // 
+            // FormKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 522);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbTen);
+            this.Controls.Add(this.lbDiaChi);
+            this.Controls.Add(this.lbSDT);
+            this.Controls.Add(this.lbHo);
+            this.Controls.Add(this.lbNS);
+            this.Controls.Add(this.lbMaKH);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormKhachHang";
+            this.Text = "FormKhachHang";
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtXoa;
+        private System.Windows.Forms.Button btSua;
+        private System.Windows.Forms.Button btThem;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button btLuu;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbTen;
+        private System.Windows.Forms.Label lbDiaChi;
+        private System.Windows.Forms.Label lbSDT;
+        private System.Windows.Forms.Label lbHo;
+        private System.Windows.Forms.Label lbNS;
+        private System.Windows.Forms.Label lbMaKH;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbKH;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
