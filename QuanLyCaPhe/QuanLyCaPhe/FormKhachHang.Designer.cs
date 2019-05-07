ff524a0458f8a8e833e76e52ceaf8fb84424de82
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-05-07 11:27:39 +0700

    Gần Hoàn Thành FormKhachHàng
    Vẫn Còn lỗi thêm khách hàng. Chiều này về tiếp tục làm.

--- QuanLyCaPhe/QuanLyCaPhe/FormKhachHang.Designer.cs
+++ QuanLyCaPhe/QuanLyCaPhe/FormKhachHang.Designer.cs
@@ -41,6 +41,9 @@
             this.lbNS = new System.Windows.Forms.Label();
             this.lbMaKH = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
             this.DgvKH = new System.Windows.Forms.DataGridView();
             this.btSua = new System.Windows.Forms.Button();
             this.btLuu = new System.Windows.Forms.Button();
@@ -48,6 +51,8 @@
             this.tbMKH = new System.Windows.Forms.TextBox();
             this.lbKH = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.cbGioiTinh = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DgvKH)).BeginInit();
             this.panel1.SuspendLayout();
@@ -58,72 +63,65 @@
             this.BtXoa.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtXoa.Image = global::QuanLyCaPhe.Properties.Resources._3Xoa;
             this.BtXoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BtXoa.Location = new System.Drawing.Point(466, 290);
-            this.BtXoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtXoa.Location = new System.Drawing.Point(600, 446);
             this.BtXoa.Name = "BtXoa";
-            this.BtXoa.Size = new System.Drawing.Size(61, 33);
+            this.BtXoa.Size = new System.Drawing.Size(92, 51);
             this.BtXoa.TabIndex = 0;
             this.BtXoa.Text = "Xóa";
             this.BtXoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.BtXoa.UseVisualStyleBackColor = true;
+            this.BtXoa.Click += new System.EventHandler(this.BtXoa_Click);
             // 
             // dtNgaySinh
             // 
-            this.dtNgaySinh.Location = new System.Drawing.Point(359, 73);
-            this.dtNgaySinh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtNgaySinh.Location = new System.Drawing.Point(538, 112);
             this.dtNgaySinh.Name = "dtNgaySinh";
-            this.dtNgaySinh.Size = new System.Drawing.Size(151, 20);
+            this.dtNgaySinh.Size = new System.Drawing.Size(224, 26);
             this.dtNgaySinh.TabIndex = 17;
             // 
             // tbDiaChi
             // 
-            this.tbDiaChi.Location = new System.Drawing.Point(359, 132);
-            this.tbDiaChi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbDiaChi.Location = new System.Drawing.Point(538, 203);
             this.tbDiaChi.Name = "tbDiaChi";
-            this.tbDiaChi.Size = new System.Drawing.Size(175, 20);
+            this.tbDiaChi.Size = new System.Drawing.Size(260, 26);
             this.tbDiaChi.TabIndex = 12;
             // 
             // tbTen
             // 
-            this.tbTen.Location = new System.Drawing.Point(141, 132);
-            this.tbTen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbTen.Location = new System.Drawing.Point(212, 203);
             this.tbTen.Name = "tbTen";
-            this.tbTen.Size = new System.Drawing.Size(122, 20);
+            this.tbTen.Size = new System.Drawing.Size(181, 26);
             this.tbTen.TabIndex = 13;
             // 
             // tbSDT
             // 
-            this.tbSDT.Location = new System.Drawing.Point(359, 106);
-            this.tbSDT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbSDT.Location = new System.Drawing.Point(538, 163);
             this.tbSDT.Name = "tbSDT";
-            this.tbSDT.Size = new System.Drawing.Size(155, 20);
+            this.tbSDT.Size = new System.Drawing.Size(230, 26);
             this.tbSDT.TabIndex = 14;
             // 
             // tbHKH
             // 
-            this.tbHKH.Location = new System.Drawing.Point(140, 101);
-            this.tbHKH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbHKH.Location = new System.Drawing.Point(210, 155);
             this.tbHKH.Name = "tbHKH";
-            this.tbHKH.Size = new System.Drawing.Size(122, 20);
+            this.tbHKH.Size = new System.Drawing.Size(181, 26);
             this.tbHKH.TabIndex = 15;
             // 
             // lbTen
             // 
             this.lbTen.AutoSize = true;
-            this.lbTen.Location = new System.Drawing.Point(47, 134);
-            this.lbTen.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbTen.Location = new System.Drawing.Point(70, 206);
             this.lbTen.Name = "lbTen";
-            this.lbTen.Size = new System.Drawing.Size(92, 13);
+            this.lbTen.Size = new System.Drawing.Size(132, 20);
             this.lbTen.TabIndex = 6;
             this.lbTen.Text = "Tên Khách Hàng:";
             // 
             // lbDiaChi
             // 
             this.lbDiaChi.AutoSize = true;
-            this.lbDiaChi.Location = new System.Drawing.Point(278, 134);
-            this.lbDiaChi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbDiaChi.Location = new System.Drawing.Point(417, 206);
             this.lbDiaChi.Name = "lbDiaChi";
-            this.lbDiaChi.Size = new System.Drawing.Size(41, 13);
+            this.lbDiaChi.Size = new System.Drawing.Size(60, 20);
             this.lbDiaChi.TabIndex = 7;
             this.lbDiaChi.Tag = "";
             this.lbDiaChi.Text = "Địa Chỉ";
@@ -131,10 +129,9 @@
             // lbSDT
             // 
             this.lbSDT.AutoSize = true;
-            this.lbSDT.Location = new System.Drawing.Point(278, 106);
-            this.lbSDT.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbSDT.Location = new System.Drawing.Point(417, 163);
             this.lbSDT.Name = "lbSDT";
-            this.lbSDT.Size = new System.Drawing.Size(78, 13);
+            this.lbSDT.Size = new System.Drawing.Size(113, 20);
             this.lbSDT.TabIndex = 8;
             this.lbSDT.Tag = "";
             this.lbSDT.Text = "Số Điện Thoại:";
@@ -142,20 +139,18 @@
             // lbHo
             // 
             this.lbHo.AutoSize = true;
-            this.lbHo.Location = new System.Drawing.Point(47, 106);
-            this.lbHo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbHo.Location = new System.Drawing.Point(70, 163);
             this.lbHo.Name = "lbHo";
-            this.lbHo.Size = new System.Drawing.Size(87, 13);
+            this.lbHo.Size = new System.Drawing.Size(126, 20);
             this.lbHo.TabIndex = 9;
             this.lbHo.Text = "Họ Khách Hàng:";
             // 
             // lbNS
             // 
             this.lbNS.AutoSize = true;
-            this.lbNS.Location = new System.Drawing.Point(278, 73);
-            this.lbNS.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbNS.Location = new System.Drawing.Point(417, 112);
             this.lbNS.Name = "lbNS";
-            this.lbNS.Size = new System.Drawing.Size(59, 13);
+            this.lbNS.Size = new System.Drawing.Size(85, 20);
             this.lbNS.TabIndex = 10;
             this.lbNS.Tag = "";
             this.lbNS.Text = "Ngày Sinh:";
@@ -163,74 +158,109 @@
             // lbMaKH
             // 
             this.lbMaKH.AutoSize = true;
-            this.lbMaKH.Location = new System.Drawing.Point(47, 73);
-            this.lbMaKH.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbMaKH.Location = new System.Drawing.Point(70, 112);
             this.lbMaKH.Name = "lbMaKH";
-            this.lbMaKH.Size = new System.Drawing.Size(91, 13);
+            this.lbMaKH.Size = new System.Drawing.Size(131, 20);
             this.lbMaKH.TabIndex = 11;
             this.lbMaKH.Text = "Mã Khách Hàng: ";
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.cbGioiTinh);
+            this.panel2.Controls.Add(this.btnThoat);
+            this.panel2.Controls.Add(this.btnReload);
+            this.panel2.Controls.Add(this.btnHuy);
             this.panel2.Controls.Add(this.DgvKH);
             this.panel2.Controls.Add(this.BtXoa);
             this.panel2.Controls.Add(this.btSua);
             this.panel2.Controls.Add(this.btLuu);
             this.panel2.Controls.Add(this.btThem);
             this.panel2.Controls.Add(this.tbMKH);
-            this.panel2.Location = new System.Drawing.Point(29, 58);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Location = new System.Drawing.Point(44, 89);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(620, 340);
+            this.panel2.Size = new System.Drawing.Size(930, 523);
             this.panel2.TabIndex = 4;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(825, 446);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(84, 51);
+            this.btnThoat.TabIndex = 32;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(721, 446);
+            this.btnReload.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(84, 51);
+            this.btnReload.TabIndex = 31;
+            this.btnReload.Text = "ReLoad";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(189, 446);
+            this.btnHuy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(84, 51);
+            this.btnHuy.TabIndex = 30;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
             // 
             // DgvKH
             // 
             this.DgvKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgvKH.Location = new System.Drawing.Point(20, 150);
-            this.DgvKH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DgvKH.Location = new System.Drawing.Point(30, 231);
             this.DgvKH.Name = "DgvKH";
             this.DgvKH.RowTemplate.Height = 28;
-            this.DgvKH.Size = new System.Drawing.Size(586, 119);
+            this.DgvKH.Size = new System.Drawing.Size(879, 183);
             this.DgvKH.TabIndex = 18;
+            this.DgvKH.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvKH_CellClick);
             // 
             // btSua
             // 
             this.btSua.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btSua.Image = global::QuanLyCaPhe.Properties.Resources._2Sua;
             this.btSua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btSua.Location = new System.Drawing.Point(347, 290);
-            this.btSua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btSua.Location = new System.Drawing.Point(455, 446);
             this.btSua.Name = "btSua";
-            this.btSua.Size = new System.Drawing.Size(56, 33);
+            this.btSua.Size = new System.Drawing.Size(84, 51);
             this.btSua.TabIndex = 0;
             this.btSua.Text = "Sửa";
             this.btSua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btSua.UseVisualStyleBackColor = true;
+            this.btSua.Click += new System.EventHandler(this.btSua_Click);
             // 
             // btLuu
             // 
             this.btLuu.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btLuu.Image = global::QuanLyCaPhe.Properties.Resources.Luu;
             this.btLuu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btLuu.Location = new System.Drawing.Point(229, 290);
-            this.btLuu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btLuu.Location = new System.Drawing.Point(314, 446);
             this.btLuu.Name = "btLuu";
-            this.btLuu.Size = new System.Drawing.Size(60, 33);
+            this.btLuu.Size = new System.Drawing.Size(90, 51);
             this.btLuu.TabIndex = 0;
             this.btLuu.Text = "Lưu";
             this.btLuu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btLuu.UseVisualStyleBackColor = true;
+            this.btLuu.Click += new System.EventHandler(this.btLuu_Click);
             // 
             // btThem
             // 
             this.btThem.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btThem.Image = global::QuanLyCaPhe.Properties.Resources._1Them;
             this.btThem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btThem.Location = new System.Drawing.Point(89, 290);
-            this.btThem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btThem.Location = new System.Drawing.Point(44, 446);
             this.btThem.Name = "btThem";
-            this.btThem.Size = new System.Drawing.Size(76, 33);
+            this.btThem.Size = new System.Drawing.Size(114, 51);
             this.btThem.TabIndex = 0;
             this.btThem.Text = "Thêm";
             this.btThem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -239,39 +269,57 @@
             // 
             // tbMKH
             // 
-            this.tbMKH.Location = new System.Drawing.Point(112, 11);
-            this.tbMKH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbMKH.Location = new System.Drawing.Point(168, 17);
             this.tbMKH.Name = "tbMKH";
-            this.tbMKH.Size = new System.Drawing.Size(122, 20);
+            this.tbMKH.Size = new System.Drawing.Size(181, 26);
             this.tbMKH.TabIndex = 16;
-            this.tbMKH.Text = "1";
             // 
             // lbKH
             // 
             this.lbKH.AutoSize = true;
             this.lbKH.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbKH.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lbKH.Location = new System.Drawing.Point(142, 8);
-            this.lbKH.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbKH.Location = new System.Drawing.Point(213, 12);
             this.lbKH.Name = "lbKH";
-            this.lbKH.Size = new System.Drawing.Size(281, 25);
+            this.lbKH.Size = new System.Drawing.Size(421, 37);
             this.lbKH.TabIndex = 0;
             this.lbKH.Text = "DANH MỤC KHÁCH HÀNG";
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.lbKH);
-            this.panel1.Location = new System.Drawing.Point(29, 15);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Location = new System.Drawing.Point(44, 23);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(620, 42);
+            this.panel1.Size = new System.Drawing.Size(930, 65);
             this.panel1.TabIndex = 5;
+            // 
+            // cbGioiTinh
+            // 
+            this.cbGioiTinh.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbGioiTinh.FormattingEnabled = true;
+            this.cbGioiTinh.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ",
+            "Không xác định"});
+            this.cbGioiTinh.Location = new System.Drawing.Point(168, 166);
+            this.cbGioiTinh.Name = "cbGioiTinh";
+            this.cbGioiTinh.Size = new System.Drawing.Size(179, 28);
+            this.cbGioiTinh.TabIndex = 34;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Giới Tính:";
             // 
             // FormKhachHang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(677, 424);
+            this.ClientSize = new System.Drawing.Size(1016, 652);
             this.Controls.Add(this.dtNgaySinh);
             this.Controls.Add(this.tbDiaChi);
             this.Controls.Add(this.tbTen);
@@ -285,7 +333,6 @@
             this.Controls.Add(this.lbMaKH);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormKhachHang";
             this.Text = "FormKhachHang";
             this.panel2.ResumeLayout(false);
@@ -320,5 +367,10 @@
         private System.Windows.Forms.TextBox tbMKH;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridView DgvKH;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbGioiTinh;
     }
 }
--- QuanLyCaPhe/QuanLyCaPhe/FormKhachHang.cs
+++ QuanLyCaPhe/QuanLyCaPhe/FormKhachHang.cs
@@ -7,19 +7,160 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QuanLyCaPhe.BSLayer;
 
 namespace QuanLyCaPhe
 {
     public partial class FormKhachHang : Form
     {
+        DataTable dt = null;
+        KhachHang kh = null;
+        bool them = false;
+        string error;
+
         public FormKhachHang()
         {
             InitializeComponent();
+            kh = new KhachHang();
+            cbGioiTinh.SelectedIndex = 0;
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+
+                dt = new DataTable();
+                dt.Clear();
+                DataSet ds = kh.LayKhachHang();
+                dt = ds.Tables[0];
+                DgvKH.DataSource = dt;
+
+                tbTen.ResetText();
+                btThem.Enabled = true;
+                btSua.Enabled = true;
+                BtXoa.Enabled = true;
+                btnReload.Enabled = true;
+                btnThoat.Enabled = true;
+                btLuu.Enabled = false;
+                btnHuy.Enabled = false;
+                cbGioiTinh.SelectedIndex = 0;
+
+            }
+            catch (Exception errr)
+            {
+                MessageBox.Show(errr.Message);
+            }
         }
 
         private void btThem_Click(object sender, EventArgs e)
         {
-            
+            them = true;
+
+            tbTen.ResetText();
+            btThem.Enabled = false;
+            btSua.Enabled = false;
+            BtXoa.Enabled = false;
+            btnThoat.Enabled = false;
+            btnReload.Enabled = false;
+            btnHuy.Enabled = true;
+            btLuu.Enabled = true;
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            btnHuy.Enabled = false;
+            btLuu.Enabled = false;
+            btThem.Enabled = true;
+            btSua.Enabled = true;
+            BtXoa.Enabled = true;
+            btnThoat.Enabled = true;
+            btnReload.Enabled = true;
+            DgvKH_CellClick(null, null);
+        }
+
+        private void btLuu_Click(object sender, EventArgs e)
+        {
+            if (!tbTen.Text.Trim().Equals(""))
+            {
+                if (them)
+                {
+                    kh.ThemKhachHang(tbMKH.Text, tbHKH.Text, tbTen.Text, tbSDT.Text, tbDiaChi.Text, cbGioiTinh.Text, dtNgaySinh.Value, ref error);
+                    // Load lại DataGridView
+                    LoadData();
+                    // THông Báo
+                    MessageBox.Show(error);
+                }
+                else
+                {
+                    int r = DgvKH.CurrentCell.RowIndex;
+                    kh.SuaKhachHang(tbMKH.Text, tbHKH.Text, tbTen.Text, tbSDT.Text, tbDiaChi.Text, cbGioiTinh.Text, dtNgaySinh.Value, ref error);
+                    // Load lại DataGridView
+                    LoadData();
+                    // THông Báo
+                    MessageBox.Show(error);
+                }
+            }
+        }
+
+        private void btSua_Click(object sender, EventArgs e)
+        {
+            them = false;
+            tbTen.ResetText();
+
+            btThem.Enabled = false;
+            btSua.Enabled = false;
+            BtXoa.Enabled = false;
+            btnThoat.Enabled = false;
+            btnReload.Enabled = false;
+            btnHuy.Enabled = true;
+            btLuu.Enabled = true;
+            DgvKH_CellClick(null, null);
+        }
+
+        private void BtXoa_Click(object sender, EventArgs e)
+        {
+            DgvKH_CellClick(null, null);
+            DialogResult kq = new DialogResult();
+            kq = MessageBox.Show("Bạn thật sự muốn xóa", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (kq == DialogResult.OK)
+            {
+                int r = DgvKH.CurrentCell.RowIndex;
+                kh.XoaKhachHang(DgvKH.Rows[r].Cells[0].Value.ToString(), ref error);
+                LoadData();
+                MessageBox.Show(error);
+            }
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult kq = new DialogResult();
+            kq = MessageBox.Show("Bạn thật sự muốn thoát", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (kq == DialogResult.OK)
+            {
+                Close();
+            }
+        }
+
+        private void DgvKH_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int r = DgvKH.CurrentCell.RowIndex;
+                tbMKH.Text = DgvKH.Rows[r].Cells[0].Value.ToString();
+                tbHKH.Text = DgvKH.Rows[r].Cells[1].Value.ToString();
+                tbTen.Text = DgvKH.Rows[r].Cells[2].Value.ToString();
+                cbGioiTinh.Text = tbTen.Text = DgvKH.Rows[r].Cells[3].Value.ToString();
+                dtNgaySinh.Value = (DateTime)DgvKH.Rows[r].Cells[4].Value;
+                tbSDT.Text = tbTen.Text = DgvKH.Rows[r].Cells[5].Value.ToString();
+                tbDiaChi.Text = DgvKH.Rows[r].Cells[6].Value.ToString();
+            }
+            catch { }         
         }
     }
 }
