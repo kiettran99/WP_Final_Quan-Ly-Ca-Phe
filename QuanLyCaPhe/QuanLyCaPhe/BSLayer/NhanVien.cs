150189ab94ac87fab80a4fa51bd9e45d5cf4b0b6
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-05-12 19:46:27 +0700

    Cập nhật lại câu SQL
    Sửa lại formnhanvien với thêm tầng nghiệp vụ thucan

--- QuanLyCaPhe/QuanLyCaPhe/BSLayer/NhanVien.cs
+++ QuanLyCaPhe/QuanLyCaPhe/BSLayer/NhanVien.cs
@@ -47,7 +47,26 @@ namespace QuanLyCaPhe.BSLayer
             dbMain.LayMa(sqlString, CommandType.Text,ref MK);
         }
 
+        public bool ThemNhanVien(string MaNV, string Ho, string TenNV, bool Nu, DateTime NgayNV, DateTime NgaySinh, string DiaChi, string SDT, ref string error)
+        {
+            string sqlString;
+            try
+            {
+                sqlString = $"Insert into NhanVien values('{MaNV.Trim()}', N'{Ho.Trim()}', N'{TenNV.Trim()}', N'{Nu}', N'{NgaySinh.ToShortDateString()}', N'{SDT.Trim()}', N'{DiaChi.Trim()}', N'{NgayNV.ToString()}')";
+            }
+            catch (SqlException)
+            {
+                error = "Thêm không được";
+                return false;
+            }
+            error = "Thêm thành công";
+            return dbMain.MyExecuteNonQuery(sqlString, CommandType.Text, ref error);
+        }
 
-
+        public bool XoaNhanVien(string MaNV, ref string error)
+        {
+            string sqlString = $"delete from NhanVien where MaNV = '{MaNV}'";
+            return dbMain.MyExecuteNonQuery(sqlString, CommandType.Text, ref error);
+        }
     }
 }
