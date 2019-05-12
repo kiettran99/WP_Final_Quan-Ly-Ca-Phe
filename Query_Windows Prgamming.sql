6fde4328e2fe47c4c2d34519f338879db2f0112c
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2019-05-12 13:56:41 +0700

    Cập nhật thêm cơ sở dữ liệu
    Cập nhất bảng gồm ThucAn, loaiThucan, banan, hoadon, chitiethoadon,

--- Query_Windows Prgamming.sql
+++ Query_Windows Prgamming.sql
@@ -1,4 +1,5 @@
-	
+--use master
+--drop database if exists QuanLyCaPhe
 create database QuanLyCaPhe
 go
 use QuanLyCaPhe
@@ -40,6 +41,49 @@ create table ThanhPho (
 	MaThanhPho int,
 	TenThanhPho varchar(20),
 	primary key(TenThanhPho)
+)
+
+create table BanAn(
+	IDBanAn int not null,					
+	TenBan nvarchar(100) not null,
+	TinhTrang nvarchar(100) not null,	-- Ban dang trong hay dang nguoi
+	primary key (IDBanAn)
+)
+
+create table LoaiThucAn(
+	IDLoaiThucAn int not null,
+	TenLoaiThucAn nvarchar(100) not null,
+	primary key (IDLoaiThucAn)
+)
+
+
+create table ThucAn(
+	IDThucAn int not null,
+	TenThucAn nvarchar(100) not null,
+	IDLoaiThucAn int not null,
+	Gia float,
+	primary key (IDThucAn),
+	foreign key (IDLoaiThucAn) references LoaiThucAn(IDLoaiThucAn)
+)
+
+create table HoaDon(
+	IDHoaDon int not null,
+	NgayTaoHoaDon datetime not null,
+	NgayKetThucHoaDon datetime,
+	IDBanAn int not null,
+	TinhTrang bit not null, -- Tinh trang thanh toan hoa don
+	primary key (IDHoaDon),
+	foreign key (IDBanAn) references BanAn(IDBanAn)
+)
+
+--Hoa don (n,n) ThucAn
+create table ChiTietHoaDon(
+	IDHoaDon int not null,
+	IDThucAn int not null,
+	SoLuong int default 0 not null,
+	primary key (IDHoaDon, IDThucAn),
+	foreign key (IDHoaDon) references HoaDon(IDHoaDon),
+	foreign key (IDThucAn) references ThucAn(IDThucAn)
 )
 
 delete from KhachHang
